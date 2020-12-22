a25fb02695251efda3a30406f67d6d8e159c0b6a
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-22 19:49:15 +0500

    Update index.js

--- red-queen-race/src/RedQueenRace/index.js
+++ red-queen-race/src/RedQueenRace/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import './style.css';
 import useWebAnimations from "@wellyshen/use-web-animations";
 
@@ -8,99 +8,99 @@ export const RedQueenRace = () => {
     var playbackrateBG = 0;
 
     /* Background animations */
-  const sceneryFrames =   [
-    { transform: 'translateX(100%)' },
-    { transform: 'translateX(-100%)' }   
-  ];
-  
-  const sceneryTimingBackground = {
-    duration: 36000,
-    iterations: Infinity,
-    playbackRate: playbackrateBG,
-  };
-  
-  const sceneryTimingForeground = {
-    duration: 12000,
-    iterations: Infinity,
-    playbackRate: playbackrateBG,
-  };
+    const sceneryFrames = [
+        { transform: 'translateX(100%)' },
+        { transform: 'translateX(-100%)' }
+    ];
 
-const background1Movement = useWebAnimations({
-    keyframes: sceneryFrames,
-    timing: sceneryTimingBackground,
-});
-  
-const background2Movement = useWebAnimations({
-    keyframes: sceneryFrames,
-    timing: sceneryTimingBackground,
-});
-  
-const foreground1Movement = useWebAnimations({
-    keyframes: sceneryFrames,
-    timing: sceneryTimingForeground
-});
-  
-const foreground2Movement = useWebAnimations({
-    keyframes: sceneryFrames,
-    timing: sceneryTimingForeground
-});
-  
-  const spriteFrames = [
-    { transform: 'translateY(0)' },
-    { transform: 'translateY(-100%)' }   
-  ];
-  
-const spirteTiming = {
-    easing: "steps(7, end)",
-    direction: "reverse",
-    duration: 600,
-    playbackRate: playbackrateRQ,
-    iterations: Infinity,
-};
+    const sceneryTimingBackground = {
+        duration: 36000,
+        iterations: Infinity,
+        playbackRate: playbackrateBG,
+    };
 
-const redQueen_alice = useWebAnimations({
-    keyframes: spriteFrames,
-    timing: spirteTiming,
-});
-  
-  const adjustBackgroundPlayback = () => {
-    if (playbackrateRQ < .8) {
-        playbackrateBG = (playbackrateRQ / 2) * -1;
-    } else if (playbackrateRQ > 1.2) {
-        playbackrateBG = (playbackrateRQ / 2);
-    } else {
-      playbackrateBG = 0;
-    }
-    foreground1Movement.getAnimation().playbackRate = playbackrateBG;   
-    foreground2Movement.getAnimation().playbackRate = playbackrateBG;   
-    background1Movement.getAnimation().playbackRate = playbackrateBG;   
-    background2Movement.getAnimation().playbackRate = playbackrateBG;   
-  };
-  
-  useEffect(()=>{
+    const sceneryTimingForeground = {
+        duration: 12000,
+        iterations: Infinity,
+        playbackRate: playbackrateBG,
+    };
 
-    const fganimation = foreground1Movement.getAnimation();
-    fganimation.currentTime = fganimation.effect.getTiming().duration / 2;
+    const background1Movement = useWebAnimations({
+        keyframes: sceneryFrames,
+        timing: sceneryTimingBackground,
+    });
 
-    const bganimation = background1Movement.getAnimation();
-    bganimation.currentTime = bganimation.effect.getTiming().duration / 2;
+    const background2Movement = useWebAnimations({
+        keyframes: sceneryFrames,
+        timing: sceneryTimingBackground,
+    });
 
-    setInterval( () => {
-        //     /* Set decay */
+    const foreground1Movement = useWebAnimations({
+        keyframes: sceneryFrames,
+        timing: sceneryTimingForeground
+    });
+
+    const foreground2Movement = useWebAnimations({
+        keyframes: sceneryFrames,
+        timing: sceneryTimingForeground
+    });
+
+    const spriteFrames = [
+        { transform: 'translateY(0)' },
+        { transform: 'translateY(-100%)' }
+    ];
+
+    const spirteTiming = {
+        easing: "steps(7, end)",
+        direction: "reverse",
+        duration: 600,
+        playbackRate: playbackrateRQ,
+        iterations: Infinity,
+    };
+
+    const redQueen_alice = useWebAnimations({
+        keyframes: spriteFrames,
+        timing: spirteTiming,
+    });
+
+    const adjustBackgroundPlayback = () => {
+        if (playbackrateRQ < .8) {
+            playbackrateBG = (playbackrateRQ / 2) * -1;
+        } else if (playbackrateRQ > 1.2) {
+            playbackrateBG = (playbackrateRQ / 2);
+        } else {
+            playbackrateBG = 0;
+        }
+        foreground1Movement.getAnimation().playbackRate = playbackrateBG;
+        foreground2Movement.getAnimation().playbackRate = playbackrateBG;
+        background1Movement.getAnimation().playbackRate = playbackrateBG;
+        background2Movement.getAnimation().playbackRate = playbackrateBG;
+    };
+
+    useEffect(() => {
+
+        const fganimation = foreground1Movement.getAnimation();
+        fganimation.currentTime = fganimation.effect.getTiming().duration / 2;
+
+        const bganimation = background1Movement.getAnimation();
+        bganimation.currentTime = bganimation.effect.getTiming().duration / 2;
+
+        setInterval(() => {
+            //     /* Set decay */
             if (playbackrateRQ > .4) {
                 playbackrateRQ *= 0.9;
-              redQueen_alice.getAnimation().playbackRate = playbackrateRQ;    
-            } 
+                redQueen_alice.getAnimation().playbackRate = playbackrateRQ;
+            }
             adjustBackgroundPlayback();
-          }, 3000);
+        }, 3000);
 
-      document.addEventListener("click", () => {
-        playbackrateRQ *= 1.1;
-        redQueen_alice.getAnimation().playbackRate *= playbackrateRQ;
-        adjustBackgroundPlayback();
-      } );
-  });
-  
+        document.addEventListener("click", () => {
+            playbackrateRQ *= 1.1;
+            redQueen_alice.getAnimation().playbackRate *= playbackrateRQ;
+            adjustBackgroundPlayback();
+        });
+    });
+
     return (
         <div className="wrapper">
             <div className="sky"></div>
