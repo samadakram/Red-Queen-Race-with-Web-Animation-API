d594b970a48a6aa520920c30e7c1d749fea5f668
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-22 12:35:27 +0500

    Update App.js

--- red-queen-race/src/App.js
+++ red-queen-race/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { RedQueenRace } from './RedQueenRace/index';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Red Queen Project</h1>
+      <RedQueenRace />
     </div>
   );
 }
