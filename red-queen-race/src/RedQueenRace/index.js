4c4adb4d999e75fe8646021bc25eb20bec050e35
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-22 19:15:34 +0500

    Update index.js

--- red-queen-race/src/RedQueenRace/index.js
+++ red-queen-race/src/RedQueenRace/index.js
@@ -121,14 +121,11 @@ const redQueen_alice = useWebAnimations({
 //     adjustBackgroundPlayback();
 //   }, 3000);
   
-  const goFaster = () => {
-    /* But you can speed them up by giving the screen a click or a tap. */
-    redQueen_alice.getAnimation().playbackRate *= 1.1;
-    // adjustBackgroundPlayback();
-  }
-  
   useEffect(()=>{
-      document.addEventListener("click", goFaster);
+      document.addEventListener("click", () => {
+        redQueen_alice.getAnimation().playbackRate *= 1.1;
+        adjustBackgroundPlayback();
+      } );
   });
   
 //   document.addEventListener("click", goFaster);
