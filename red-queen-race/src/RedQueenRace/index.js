c7b8cacb044c33614876d5cb9cbb39c05d12f7ed
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-22 18:42:13 +0500

    Update index.js

--- red-queen-race/src/RedQueenRace/index.js
+++ red-queen-race/src/RedQueenRace/index.js
@@ -134,7 +134,7 @@ const redQueen_alice = useWebAnimations({
             <div className="sky"></div>
             <div className="earth">
                 <div id="red-queen_and_alice">
-                    <img id="red-queen_and_alice_sprite" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png" srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen.png 2x" alt="Alice and the Red Queen running to stay in place." />
+                    <img ref={redQueen_alice.ref} id="red-queen_and_alice_sprite" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png" srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen.png 2x" alt="Alice and the Red Queen running to stay in place." />
                 </div>
 
                 <div className="scenery" id="foreground1" ref={foreground1Movement.ref}>
